2577a31b533d9153c728f425eb985dc2f42f6f94
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2025-11-25 08:52:09 +0530

    Add initial comment for largest odd number problem

---
+++ string10.cpp
@@ -0,0 +1,16 @@
+
+//1903. Largest odd number in string
+
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        for (int i = n; i>=0;i--){
+            if(int(num[i]) % 2 != 0){
+            return num.substr(0,i+1);
+            }
+        }
+        return "";
+    }
+};
