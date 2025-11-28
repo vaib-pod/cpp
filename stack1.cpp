78bf401e8e6f085b94c725654166418d0990add6
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2025-11-28 18:59:37 +0530

    Create stack1.cpp
    Implement a solution to validate parentheses using a stack.

---
+++ stack1.cpp
@@ -0,0 +1,21 @@
+//20. Valid parenthesis using stack
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(auto ch : s){
+
+            if(ch=='(' ||  ch=='[' || ch=='{' ){
+                st.push(ch);
+            }
+            else if(!st.empty() && (ch == ')' && st.top()=='(' || ch == ']' && st.top()=='[' || ch == '}' && st.top()=='{' )){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};
