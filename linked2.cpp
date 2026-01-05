f6a623dde64a56a565dd7b0ac7e4fe993935a48d
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-01-05 21:46:16 +0530

    Create linked2.cpp

---
+++ linked2.cpp
@@ -0,0 +1,13 @@
+//237. Delete node in a linked list 
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        ListNode* temp = node->next;
+        node->next = node->next->next;
+        delete temp;
+        
+    }
+
+};
