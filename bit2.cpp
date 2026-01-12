0cf5333651659fcfd049e22bc487560c083a5dde
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-01-12 18:11:47 +0530

    Create bit2.cpp

---
+++ bit2.cpp
@@ -0,0 +1,17 @@
+//222-. Minimum bit flips to convert number
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0 ;
+        while(start!=goal){
+            if((start&1)!=(goal&1)){
+                count++;
+            }
+            start = start >> 1;
+            goal = goal >> 1;
+        }
+
+        return count;
+    }
+};
