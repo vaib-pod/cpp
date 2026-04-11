3eb72fe001b885bb1d4cce8e690eaf976b3983e4
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-04-11 13:08:26 +0530

    Implement twoSum function in arrr28.cpp

---
+++ arrr28.cpp
@@ -0,0 +1,29 @@
+//1. two sum
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int,int>> arr;
+        for(int i = 0 ; i < n ; i ++){
+            arr.push_back({nums[i],i});
+        }
+        sort(arr.begin(),arr.end());
+        int left = 0;
+        int right = n-1;
+        while(left<right){
+            int sum = arr[left].first + arr[right].first;
+            if(sum==target){
+                return {arr[left].second, arr[right].second};
+            }
+            else if(sum>target){
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        return {};
+    }
+};
