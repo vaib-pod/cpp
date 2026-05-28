b1dd49258f18fe64f04754ff1730ba3a10087e05
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-05-28 18:50:07 +0530

    Create arr31.cpp

---
+++ arr31.cpp
@@ -0,0 +1,42 @@
+//18. 4 sum
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> result;
+        if (n < 4) {
+            return result;
+        }
+        for(int i = 0 ; i < n ; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for(int j = i + 1; j<n;j++){
+                if (j > i+1 && nums[j] == nums[j - 1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum==target){
+                        result.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        while(k<l && nums[k]==nums[k+1]){
+                            k++;
+                        }
+                        while(k<l && nums[l]==nums[l-1]){
+                            l--;
+                        }
+                        l--;
+                        k++;
+                    }
+                    else if(sum<target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
