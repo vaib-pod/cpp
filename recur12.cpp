8d56ede77547ef5c4c18a3297033bb9db0bd4eee
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-06-10 13:34:35 +0530

    Implement subsets with duplicates in recur12.cpp

---
+++ recur12.cpp
@@ -0,0 +1,22 @@
+//90/. subsets II
+
+class Solution {
+public:
+    void findcombo(vector<int>& nums , int ind , vector<int>& ds,vector<vector<int>>& ans){
+        ans.push_back(ds);
+        int n = nums.size();
+        for(int i = ind; i<n;i++ ){
+            if(i>ind && nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            findcombo(nums,i+1,ds,ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> ds;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        findcombo(nums,0,ds,ans);
+        return ans;
+    }
+};
