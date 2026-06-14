e730c1f7a2c681cfdb980a08f6b15b9c54ab1caf
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-06-14 17:48:44 +0530

    Create bst2.cpp

---
+++ bst2.cpp
@@ -0,0 +1,44 @@
+//701. insert into a binary search tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode* temp = root;
+        if (root == nullptr) {
+            return new TreeNode(val);
+        }
+        while(true){
+            if(val<temp->val){
+                if(temp->left == NULL){ 
+                    temp->left =  new TreeNode(val);
+                    break; 
+                }
+                else{
+                    temp = temp->left;
+                }
+            }
+            else{
+                if(temp->right == NULL){ 
+                    temp->right =  new TreeNode(val);
+                    break; 
+                }
+                else{
+                    temp = temp->right;
+                }
+            }
+        }
+        
+        return root;
+    }
+};
