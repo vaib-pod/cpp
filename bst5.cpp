1e3850c80d19fdea08eeb3cae73bd37fc81bd350
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-06-16 15:12:28 +0530

    Create bst5.cpp

---
+++ bst5.cpp
@@ -0,0 +1,44 @@
+//99. recover binary search tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* first; 
+    TreeNode* last; 
+    TreeNode* middle; 
+    TreeNode* prev; 
+    void inorder(TreeNode* node){
+        if(node==NULL){
+            return;
+        }
+        inorder(node->left);
+        if(prev!=NULL && (node->val<prev->val)){
+            if(first==NULL){
+                first = prev;
+                middle = node;
+            }
+            else{
+                last = node;
+            }
+        }
+        prev = node;
+        inorder(node->right);
+    }
+    void recoverTree(TreeNode* root) {
+        first=middle=last=NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last) swap(first->val , last->val);
+        else if(first && middle) swap(first->val,middle->val);
+    }
+};
