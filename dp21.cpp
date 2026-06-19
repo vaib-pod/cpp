8808e03878e80017a33d995d2c66f7d3131fc9fd
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-06-19 19:51:18 +0530

    Create dp21.cpp

---
+++ dp21.cpp
@@ -0,0 +1,23 @@
+//1312. minimum insertion steps to make a string palindrome
+
+class Solution {
+public:
+    int f(int ind1, int ind2, string &text1, string &text2,vector<vector<int>> &dp){
+        int n1 = text1.size();
+        int n2 = text2.size();
+        if(ind1==n1 || ind2==n2) return 0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(text1[ind1]==text2[ind2]){
+            return dp[ind1][ind2] = 1+ f(ind1+1,ind2+1,text1,text2,dp);
+        }
+        return dp[ind1][ind2] = max(f(ind1+1,ind2,text1,text2,dp),f(ind1,ind2+1,text1,text2,dp));
+    }
+    int minInsertions(string s) {
+        string text1 = s;
+        reverse(s.begin(),s.end());
+        string text2 = s;
+        int n = s.size();
+        vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1)); 
+        return n- f(0,0,text1,text2,dp);
+    }
+};
