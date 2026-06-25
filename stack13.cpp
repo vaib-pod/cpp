b36f91e23c404a8b02f765e8378bd6b64c8b0e97
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-06-25 16:39:43 +0530

    Add sliding window maximum solution
    Implement sliding window maximum using deque.

---
+++ stack13.cpp
@@ -0,0 +1,24 @@
+//239. sliding window maximum 
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> results;
+        deque<int> dq;
+
+        for(int i = 0 ; i< nums.size();i++){
+            if(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[i]>nums[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k-1){
+                results.push_back(nums[dq.front()]);
+            }
+        }
+
+        return results;
+    }
+};
