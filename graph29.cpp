46ed3b314c6f35eb717989ab3f58116327a1a1ba
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-06-30 18:13:30 +0530

    Create graph29.cpp

---
+++ graph29.cpp
@@ -0,0 +1,75 @@
+//1319. number of operations to make network connected
+
+class DisjointSet {
+   
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int count = 0;
+        DisjointSet ds(n);
+        for(auto it : connections){
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUPar(u)==ds.findUPar(v)) count++;
+            else ds.unionBySize(u,v);
+        }
+
+        int comp = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(ds.parent[i]==i) comp++;
+        }
+        int ans = comp-1;
+        if(count>=ans) return ans;
+        return -1;
+
+
+    }
+};
