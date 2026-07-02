315eb68b971bbbe5bbb05bd86e5d550a45ddc896
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-07-02 16:57:02 +0530

    Create string21.cpp

---
+++ string21.cpp
@@ -0,0 +1,24 @@
+//647. palindromic substrings
+
+class Solution {
+public:
+    bool isPalindrome(int i ,int j ,string&s){
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;j--;
+        }
+        return true;
+    }
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count=0;
+        for(int i = 0 ; i < n;i++){
+            for(int j = i;j<n;j++){
+                if(isPalindrome(i,j,s)) count++;
+            }
+        }
+        return count;
+    }
+};
