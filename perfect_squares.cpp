e4b63d25977e89f27fefe34e1d2b299f027eacad
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-07-20 17:06:32 +0530

    GeetCode Sync: Perfect Squares

---
+++ perfect_squares.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(int n,vector<int>&dp){
+        if(n==0) return 0;
+        int minval = INT_MAX;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        for(int i = 1 ; i*i<=n;i++){
+            int result = 1+solve(n-i*i,dp);
+            minval = min(minval, result);
+        }
+        return dp[n] = minval;
+    }
+    int numSquares(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
