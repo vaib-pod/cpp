1d51eefcae1facd4d450831a6dd7d55bf0db4b49
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-07-22 09:41:30 +0530

    GeetCode Sync: Removing Stars From a String

---
+++ removing_stars_from_a_string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+        for(int i = 0 ; i< s.size();i++){
+            if(s[i]=='*'){
+                st.pop();
+            }    
+            else{
+                st.push(s[i]);
+            }   
+        }
+
+        string result;
+        while(!st.empty()){
+            result+=st.top();
+            st.pop();
+        }
+        reverse(result.begin(),result.end());
+        return result;
+        
+    }
+};
