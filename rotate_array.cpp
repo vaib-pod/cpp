235f40d4432489ccb966eea404887990a6b813f1
Vaibhav Poddar <vaibhavpoddar1@gmail.com>
2026-07-22 10:32:53 +0530

    GeetCode Sync: Rotate Array

---
+++ rotate_array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> arr;
+        if(k>n){
+            k=k%n;
+        }
+        for(int i = n-k;i<n;i++){
+            arr.push_back(nums[i]);
+        }
+        for(int j = 0;j<n-k;j++){
+            arr.push_back(nums[j]);
+        }
+        nums.swap(arr);
+    }
+};
